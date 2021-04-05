118dff4a8d750d5a62989ca5fcc4bac0f5174e2e
Tony Miller <mcfiredrill@gmail.com>
2021-04-05 16:24:07 +0900

    use tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,15 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    minifyCSS: {
+      enabled: false,
+    },
+
+    postcssOptions: {
+      compile: {
+        plugins: [require('tailwindcss')('./app/tailwind/config.js')],
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +28,9 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/netlify.toml', {
+    destDir: '/',
+  });
 
   return app.toTree();
 };
